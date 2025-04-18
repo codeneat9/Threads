3ce5cc3d9adfc28d49050900f324aab7a50fec25
codeneat9 <yashwithbehara18@gmail.com>
2025-04-18 23:42:18 +0530

    Thread using Thread class

--- ThreadManager.java
+++ ThreadManager.java
@@ -3,3 +3,7 @@ public class ThreadManager {
     public void runAllThreads() {
         System.out.println("=== Starting Threads ===");
 
+        // Thread using Thread class
+        MyThread thread1 = new MyThread("Thread-Class");
+        thread1.start();
+
