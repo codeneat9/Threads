339b2e3b0bba7e9a1eeb17b15bb187bd46d82821
codeneat9 <yashwithbehara18@gmail.com>
2025-04-18 23:40:54 +0530

    Displaying the thread activity and handling the exception for MyRunnable class

--- MyRunnable.java
+++ MyRunnable.java
@@ -6,3 +6,14 @@ public class MyRunnable implements Runnable {
     }
 
     public void run() {
+        // Display current thread activity
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(taskName + " - Count: " + i);
+            try {
+                Thread.sleep(700); // pause for 0.7 seconds
+            } catch (InterruptedException e) {
+                System.out.println(taskName + " interrupted.");
+            }
+        }
+    }
+}
