b113c4db9ba6130f6482be7376957ee489f58df2
codeneat9 <yashwithbehara18@gmail.com>
2025-04-18 23:36:12 +0530

    Calling the function for thread execution

--- Main.java
+++ Main.java
@@ -5,3 +5,8 @@ Batch: AIML B3
 */
 
 public class Main {
+    public static void main(String[] args) {
+        ThreadManager manager = new ThreadManager();
+        manager.runAllThreads(); // calling function to demonstrate thread execution
+    }
+}
